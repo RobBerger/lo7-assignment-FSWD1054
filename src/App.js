96ee1f4e3c96430a4ed0085c2ebdf1d645320df7
berger <robertberger.dev@gmail.com>
2023-03-03 16:45:51 -0500

    imported Home into App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserContext from './UserContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
