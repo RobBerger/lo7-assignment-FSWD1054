b4409f6310ada0240dfb620743eddb76e4e33a28
berger <robertberger.dev@gmail.com>
2023-03-03 16:55:25 -0500

    added return code to SignInForm

--- src/SignInForm.js
+++ src/SignInForm.js
@@ -4,7 +4,21 @@ function SignInForm() {
     let [ credentials, setCredentials ] = useState({
         username: "",
         password: ""
-      })
+      });
+
+      return (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" >
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" name="username" value={username} onChange={handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name="password" value={password} onChange={handleChange} />
+          </Form.Group>
+          <Button type="submit">Sign In</Button>
+        </Form>
+      );
 }
 
 export default SignInForm;
