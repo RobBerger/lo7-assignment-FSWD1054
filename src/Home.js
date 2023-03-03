6bf48f2866d9e8bdc80f9fd82a450434291613fe
berger <robertberger.dev@gmail.com>
2023-03-03 17:09:49 -0500

    imported bootstrap items into Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Stack from "react-bootstrap/Stack";
 
 function Home() {
     return (
