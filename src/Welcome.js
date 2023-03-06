bccffc8c6878b40c47f2af658f37d45c9e3ab27b
berger <robertberger.dev@gmail.com>
2023-03-06 12:08:39 -0500

    set up Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Welcome() {
-
+    return (
+        <>
+        <h1>Welcome</h1>
+        <p>Hello! Thanks for visitin our website!</p>
+        </>
+    )
 }
 
 export default Welcome;
